0827d82fbeb674e199c921edb6262a088d9ef7a7
Jon Leithe <jon@jonleithe.no>
2025-09-01 10:44:46 +0200

    Commit from terminal!

--- git-init.sh
+++
@@ -1,7 +0,0 @@
-echo "# ivs_sessions_browser" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M master
-git remote add origin git@github.com:faksdal/ivs_sessions_browser.git
-git push -u origin master
