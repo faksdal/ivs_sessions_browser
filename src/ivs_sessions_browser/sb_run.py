d0ec57f4d032d0c72fc5778405b092dd2c723702
Jon Leithe <jon@jonleithe.no>
2026-01-24 09:34:42 +0100

    Moving class methods into separate files; for easier maintenance

---
+++ src/ivs_sessions_browser/sb_run.py
@@ -0,0 +1,19 @@
+
+
+
+from __future__ import annotations
+
+from .defs import SessionsBrowserLike
+
+
+class SessionsBrowserRunMixin:
+    def run(self: SessionsBrowserLike):
+        """
+        Defined in sb_run.py.
+        Docstring for render_sessions_list
+
+
+        :return:
+        """
+        print("SessionsBrowserRunMixin::run() called")
+# ─── END OF run() ─────────────────────────────────────────────────────────────
